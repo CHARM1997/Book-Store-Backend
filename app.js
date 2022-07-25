74394d4b5f6c088c7908d5fc9d5278be93ee982a
it18208436 <it18208436@my.sliit.lk>
2022-07-25 14:47:40 +0530

    npm cors intalled

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require("./routes/book-routes");
 const app = express();
+const cors = require('cors');
 
 //Middliwares 
 /*app.use("/", (req, res, next)=>{
     res.send("This is our starting app")
 })*/
 app.use(express.json());
+app.use(cors());
 app.use("/books",router)//localhost:5000/books  
 
 mongoose.connect("mongodb+srv://admin:ultimate10@cluster0.kj8cvtx.mongodb.net/bookStore?retryWrites=true&w=majority"
